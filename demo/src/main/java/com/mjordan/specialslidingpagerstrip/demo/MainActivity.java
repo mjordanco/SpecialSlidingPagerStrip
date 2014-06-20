e442e08baee481f67ec5f6c5d2fc7d2cd4ce4202
Matthew <matthewjordanco@gmail.com>
2014-06-20 15:46:08 -0600

    Initial Commit

---
+++ demo/src/main/java/com/mjordan/specialslidingpagerstrip/demo/MainActivity.java
@@ -0,0 +1,68 @@
+package com.mjordan.specialslidingpagerstrip.demo;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    private ViewPager mViewPager;
+    private SlidingPagerStrip mPagerStrip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mPagerStrip = (SlidingPagerStrip) findViewById(R.id.sliding_pager_strip);
+
+        mViewPager.setAdapter(new BogusPagerAdapter(getFragmentManager()));
+        mPagerStrip.setViewPager(mViewPager);
+    }
+
+    private static class BogusPagerAdapter extends FragmentPagerAdapter {
+
+        public BogusPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new BogusFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 12;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Titles!";
+        }
+    }
+
+    public static class BogusFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_bogus, container, false);
+            Random rand = new Random();
+            view.setBackgroundColor(Color.rgb(rand.nextInt(365), rand.nextInt(365), rand.nextInt(365)));
+
+            return view;
+        }
+    }
+}
---
+++ demo/src/main/java/com/mjordan/specialslidingpagerstrip/demo/SlidingPagerStrip.java
@@ -0,0 +1,191 @@
+package com.mjordan.specialslidingpagerstrip.demo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by matt on 6/19/14.
+ */
+public class SlidingPagerStrip extends HorizontalScrollView implements ViewPager.OnPageChangeListener, View.OnClickListener{
+
+    private LinearLayout mTabLayout;
+    private ViewPager mViewPager;
+    private PagerAdapter mAdapter;
+    private LayoutInflater mLayoutInflater;
+
+    private Path mPath;
+    private Paint mPaint;
+
+    private List<TextView> mTitles;
+
+    private int mCurrentPage;
+    private float mOffset;
+
+    private int mPathOffset;
+
+    public SlidingPagerStrip(Context context) {
+        this(context, null);
+    }
+
+    public SlidingPagerStrip(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SlidingPagerStrip(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        setPadding(0, 20, 0, 20);
+
+        mOffset = 0;
+
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        mPath = new Path();
+        mPath.lineTo(20, 20);
+        mPath.lineTo(40, 0);
+        mPath.lineTo(0, 0);
+
+        mPathOffset = 0;
+
+        setEnabled(false);
+
+        mLayoutInflater = LayoutInflater.from(context);
+        mTitles = new ArrayList<TextView>();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        if (getChildCount() > 0) {
+            throw new IllegalArgumentException("SlidingPagerStrip cannot be inflated with children.");
+        }
+
+        mLayoutInflater.inflate(R.layout.view_sliding_power_strip, this);
+
+        mTabLayout = (LinearLayout) findViewById(R.id.tab_layout);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        TextView title = mTitles.get(mCurrentPage);
+
+        int center = (int) (title.getLeft() + title.getWidth() / 2 + mOffset * title.getWidth());
+
+        mPath.offset(-mPathOffset, 0);
+        mPath.offset(center - 20, 0);
+        mPathOffset = center - 20;
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+
+        if (changed) {
+            for (TextView tv : mTitles) {
+                int width = Math.max(getMeasuredWidth() / mAdapter.getCount(), getMeasuredWidth() / 4);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT);
+                tv.setLayoutParams(params);
+            }
+        }
+    }
+
+    public void setViewPager(ViewPager viewPager) {
+        mViewPager = viewPager;
+        mViewPager.setOnPageChangeListener(this);
+
+        mAdapter = mViewPager.getAdapter();
+
+        mTabLayout.removeAllViews();
+        mTitles.clear();
+
+        for (int i = 0; i < mAdapter.getCount(); i++) {
+            TextView title = new TextView(getContext());
+            title.setTextColor(Color.BLACK);
+            title.setTextSize(18f);
+            title.setText(mAdapter.getPageTitle(i));
+            title.setGravity(Gravity.CENTER_HORIZONTAL);
+            title.setOnClickListener(this);
+
+            title.setTag(i);
+
+            mTabLayout.addView(title);
+
+            mTitles.add(title);
+        }
+
+        mCurrentPage = 0;
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        TextView title = mTitles.get(position);
+
+        boolean scrollingRight =  mCurrentPage == position;
+
+        if (scrollingRight) {
+            if (position > 0) {
+                smoothScrollTo((int) (title.getLeft() - title.getWidth() + positionOffset * title.getWidth()), 0);
+            }
+        }
+
+
+        mCurrentPage = position;
+        mOffset = positionOffset;
+
+        invalidate();
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
+
+    @Override
+    public void onClick(View v) {
+        mViewPager.setCurrentItem((Integer) v.getTag());
+    }
+}
